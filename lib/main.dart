c1b2e259cac5d83016bcebf09c1edca0888385ef
Youri Chcouropat <youri.chcouropat.ext@orange.com>
2020-03-28 12:36:23 +0100

    Update dice

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,62 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Challenge Dés',
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
-      ),
-      home: MyHomePage(title: 'Extreme Fury Rolling Dice 2000'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final Random random = new Random();
-  int _indexDice1 = 1;
-  int _indexDice2 = 1;
-
-  int _randomDice() {
-    setState(() {
-      _indexDice1 = random.nextInt(6) + 1;
-      _indexDice2 = random.nextInt(6) + 1;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.orange,
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Expanded(
-                child: GestureDetector(
-              onTap: _randomDice,
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
-                child: Image.asset('images/dice' + '$_indexDice1' + '.png'),
-              )
-            )),
-            Expanded(
-                child: GestureDetector(
-              onTap: _randomDice,
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
-                child: Image.asset('images/dice' + '$_indexDice2' + '.png')
-              ),
-            )),
-          ],
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
         ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  leftDiceNumber = Random().nextInt(6) + 1;
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                });
+              },
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                });
+              },
+              child: Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Image.asset('images/dice$rightDiceNumber.png')),
+            ),
+          ),
+        ],
       ),
     );
   }
